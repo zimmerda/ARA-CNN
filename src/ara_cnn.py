bd28bda5918b59a0a61512a05b841a8d5682b3ec
David Niklas Zimmer <95021195+zimmerda@users.noreply.github.com>
2022-10-15 11:46:34 +0200

    Update ara_cnn.py

--- src/ara_cnn.py
+++ src/ara_cnn.py
@@ -3,7 +3,6 @@
 import os
 import logging
 import argparse
-import keras
 
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, TensorBoard, Callback
 from keras.layers import Input, GlobalAveragePooling2D, add, AveragePooling2D, Dense, Dropout
